720a929d49f66c17f1dfe7a0a2684bedaf0ab4fa
Dmitrii <dimitrii-07@mail.ru>
2023-04-23 21:28:21 +0300

    Вторая часть 3 задания

--- file/setup/setup/src/Main.java
+++ file/setup/setup/src/Main.java
@@ -1,11 +1,14 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Main {
     public static void main(String[] args) {
         var stringBuilder = new StringBuilder();
         var url = "Z:\\Users\\Работа\\Desktop\\Game";
+        var gameProgress1 = new GameProgress(10, 10, 1, 5.5);
+        var gameProgress2 = new GameProgress(15, 15, 2, 4.5);
+        var gameProgress3 = new GameProgress(20, 20, 3, 3.5);
         stringBuilder.append(url + "\\src Create " + new File(url + "\\src").mkdir() + "\n");
         stringBuilder.append(url + "\\res Create " + new File(url + "\\res").mkdir() + "\n");
         stringBuilder.append(url + "\\savegames Create " + new File(url + "\\savegames").mkdir() + "\n");
@@ -29,12 +32,47 @@ public class Main {
             var file = new File(url);
             stringBuilder.append(url + " Create " + file.createNewFile() + "\n");
             var fileWriter = new FileWriter(file);
-            fileWriter.write( stringBuilder.toString());
+            fileWriter.write(stringBuilder.toString());
             fileWriter.flush();
             fileWriter.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        System.out.println(stringBuilder);
+        url = "Z:\\Users\\Работа\\Desktop\\Game\\savegames";
+
+        saveGame(url + "\\save1.dat", gameProgress1);
+        saveGame(url + "\\save2.dat", gameProgress2);
+        saveGame(url + "\\save3.dat", gameProgress3);
+        zipSave(url + "\\");
+    }
+
+    private static void saveGame(String url, GameProgress gameProgress) {
+        try (var fileOutputStream = new FileOutputStream(url)) {
+            var objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(gameProgress);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void zipSave(String url) {
+        String[] fileList = new File(url).list();
+        for (String fileUrl : fileList) {
+            if (fileUrl.contains(".dat")) {
+                try (var fileInputStream = new FileInputStream(url + fileUrl);
+                     var zipOutputStream = new ZipOutputStream(new FileOutputStream(url + fileUrl.replace(".dat", ".zip")))) {
+                    byte[] buffer = new byte[fileInputStream.available()];
+                    var entry = new ZipEntry(fileUrl);
+                    zipOutputStream.putNextEntry(entry);
+                    fileInputStream.read(buffer);
+                    zipOutputStream.write(buffer);
+                    zipOutputStream.closeEntry();
+                    fileInputStream.close();
+                    new File(url + fileUrl).delete();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
